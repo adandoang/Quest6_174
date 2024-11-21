94e991bc6be1a95371d4f96280feb314d10aac00
adandoang <akhdanjzif@gmail.com>
2024-11-21 21:59:12 +0700

    membuat variabel list data dan membuat column serta peletakan posisi oleh modifier

--- app/src/main/java/com/example/meeting8/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/meeting8/ui/view/screen/RencanaStudyView.kt
@@ -1,10 +1,16 @@
 package com.example.meeting8.ui.view.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import com.example.meeting8.R
 import com.example.meeting8.model.Mahasiswa
 import com.example.meeting8.model.RencanaStudi
 
@@ -21,4 +27,11 @@ fun RencanaStudyView(
     }
     var checked by remember { mutableStateOf(false) }
     var pilihanKelas by remember { mutableStateOf("") }
+
+    var listData: MutableList<String> = mutableListOf(chosenDropdown, pilihanKelas)
+    Column (modifier = Modifier
+        .fillMaxSize()
+        .background(color = colorResource(id = R.color.greenlatern))){
+
+    }
 }
