fd246944d254bb52fba9882c34831643096d0a98
adandoang <akhdanjzif@gmail.com>
2024-11-21 21:55:59 +0700

    membuat variebel chosen dropdown, checked, dan pilihan kelas

--- app/src/main/java/com/example/meeting8/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/meeting8/ui/view/screen/RencanaStudyView.kt
@@ -1,12 +1,24 @@
 package com.example.meeting8.ui.view.screen
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import com.example.meeting8.model.Mahasiswa
 import com.example.meeting8.model.RencanaStudi
 
+@Composable
 fun RencanaStudyView(
     mahasiswa: Mahasiswa,
     onSubmitButtonClicked: (MutableList<String>) -> Unit,
     onBackButtonClicked: () -> Unit
 ) {
-
+    var chosenDropdown by remember {
+        mutableStateOf(
+            ""
+        )
+    }
+    var checked by remember { mutableStateOf(false) }
+    var pilihanKelas by remember { mutableStateOf("") }
 }
