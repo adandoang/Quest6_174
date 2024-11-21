581a000dd017fa993c303f88714e2cc990589854
adandoang <akhdanjzif@gmail.com>
2024-11-21 23:18:02 +0700

    membuat view model untuk rencana study agar terhubung vie dan modelny

---
+++ app/src/main/java/com/example/meeting8/ui/view/viewmodel/RencanaStudyViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.meeting8.ui.view.viewmodel
+
+import android.view.View
+import androidx.lifecycle.ViewModel
+import com.example.meeting8.model.RencanaStudi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class RencanaStudyViewModel : ViewModel() {
+    private val _krsState = MutableStateFlow(RencanaStudi())
+    val krsStateUi: StateFlow<RencanaStudi> = _krsState.asStateFlow()
+
+    fun setMataKuliah(mkPilihan: String) {
+        _krsState.update { stateMK -> stateMK.copy(namaMK = mkPilihan) }
+    }
+
+
+}
