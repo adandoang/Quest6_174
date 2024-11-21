20d727531472f86c8395bf77f402ec1aefac6eab
adandoang <akhdanjzif@gmail.com>
2024-11-21 21:53:13 +0700

    membuat fungsi rencana study view dan memberikannya parameter yang dibutuhkan

---
+++ app/src/main/java/com/example/meeting8/ui/view/screen/RencanaStudyView.kt
@@ -0,0 +1,12 @@
+package com.example.meeting8.ui.view.screen
+
+import com.example.meeting8.model.Mahasiswa
+import com.example.meeting8.model.RencanaStudi
+
+fun RencanaStudyView(
+    mahasiswa: Mahasiswa,
+    onSubmitButtonClicked: (MutableList<String>) -> Unit,
+    onBackButtonClicked: () -> Unit
+) {
+
+}
